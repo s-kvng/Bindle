efe11fa4c6fd34e867168a8b51b85106d7f77399
nathan <kvngnathan8420@gmail.com>
2023-08-08 06:23:48 +0000

    setup auth system for app

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import '../globals.css'
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: {children:React.ReactNode}){
         <ClerkProvider>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
+                    
                     {children}
                 </body>
             </html>
