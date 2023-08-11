14b2234487951782ea7fb67a7a872ccfd261ad78
s-kvng <kvngnathan8420@gmail.com>
2023-08-11 01:35:00 +0000

    update leftsideBar & add contants

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,11 @@
 function LeftSidebar(){
-    return <h1>LeftSidebar</h1>
+    return(
+        <section className=" custom-scrollbar leftsidebar ">
+            <div className=" flex flex-1 flex-col w-full gap-6 px-6  ">
+            
+            </div>
+        </section>
+    )
 }
 
 export default LeftSidebar;
